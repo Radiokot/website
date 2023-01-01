dd7c416b5ae9e35372c16d37d8094a6b7d0dac98
Oleg Koretsky <oleg@radiokot.com.ua>
2023-01-01 19:37:06 +0200

    Convert header images to webp

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ var HEADER_BACKGROUNDS_COUNT = 6;
 var setRandomHeaderBackground = function () {
     var fileNumber = 1 + Math.floor(Math.random() * 100) % HEADER_BACKGROUNDS_COUNT
     var header = document.getElementById('header')
-    header.style.background = '#2d241d url("assets/img/header/' + fileNumber + '.jpg") 100% 100% no-repeat'
+    header.style.background = '#2d241d url("assets/img/header/' + fileNumber + '.webp") 100% 100% no-repeat'
 	header.style.backgroundPosition = 'center'
 	header.style.backgroundSize = 'cover'
 }
