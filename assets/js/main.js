204e82779432ac482b56d2d1b85a9c3793a5f9cb
Oleg Koretsky <oleg@radiokot.com.ua>
2023-04-14 16:26:23 +0300

    Add the tip page

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,26 @@ var setRandomHeaderBackground = function () {
     var fileNumber = 1 + Math.floor(Math.random() * 100) % HEADER_BACKGROUNDS_COUNT
     var header = document.getElementById('header')
     header.style.background = '#2d241d url("assets/img/header/' + fileNumber + '.webp") 100% 100% no-repeat'
-	header.style.backgroundPosition = 'center'
-	header.style.backgroundSize = 'cover'
+    header.style.backgroundPosition = 'center'
+    header.style.backgroundSize = 'cover'
+}
+
+var processCollapsibles = function () {
+    var collapsibles = document.getElementsByClassName('collapsible')
+    for (const collapsible of collapsibles) {
+        const opener = collapsible.getElementsByClassName('collapsible-opener')[0]
+        const content = collapsible.getElementsByClassName('collapsible-content')[0]
+
+        if (opener && content) {
+            opener.onclick = (e) => {
+                e.preventDefault()
+                if (content.classList.toggle('collapsible-content-expanded')) {
+                    collapsible.scrollIntoView({block: 'start', behavior: 'smooth'})
+                }
+            }
+        }
+    }
 }
 
 setRandomHeaderBackground()
+processCollapsibles()
